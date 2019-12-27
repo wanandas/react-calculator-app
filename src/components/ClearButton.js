56a7f7a9a5b0efa2ac7b7585b093772521dcd9a6
unknown <wanzerox@gmail.com>
2019-12-28 04:53:15 +0700

    add children clear-btn

--- src/components/ClearButton.js
+++ src/components/ClearButton.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./ClearButton.css";
 
 const ClearButton = props => {
-  return <div className="clear-btn" onClick={() => props.handleClear()}></div>;
+  return (
+    <div className="clear-btn" onClick={() => props.handleClear()}>
+      {props.children}
+    </div>
+  );
 };
 
 export default ClearButton;
